6ec1121357debe86e6f5f23dd0f0875dfaf26b87
giocast <42517816+giocast@users.noreply.github.com>
2021-01-13 21:14:45 +0100

    Update connessioneWiFiMQTT.py

--- connessioneWiFiMQTT.py
+++ connessioneWiFiMQTT.py
@@ -27,11 +27,43 @@ client.subscribe(topic="bldg/feeds/bldgdata")
 
 while True:
     print("Sending value")
-
+    #imp diff tra (tuple) e [list] -> posso accedere a tuple con tupla[2] ma non a lista (lista si append)
     dataInizio=utime.localtime()
     time.sleep(2) #ipotetico intervallo
     dataFine=utime.localtime()
-    durataIntervallo=str(dataFine[5]-dataInizio[5])
+    durataInt=[(dataFine[0]-dataInizio[0]),(dataFine[1]-dataInizio[1]),(dataFine[2]-dataInizio[2]),(dataFine[3]-dataInizio[3]),(dataFine[4]-dataInizio[4]),(dataFine[5]-dataInizio[5])]
+    #durataInt.append(dataFine[0]-dataInizio[0])
+    #durataInt.append(dataFine[1]-dataInizio[1])
+
+    #for per scandire
+    # se prendo elemento !=0 devo moltiplicare per ottenere il valore in secondi
+
+    #for elemento in durataInt:
+    #    if elemento != 0:
+    length = len(durataInt)
+    i = 0
+    sommaPeriodoSecondi = 0
+
+    while i < length:
+        if durataInt[i] != 0:
+
+            if i == 3:
+                #ora
+                print("prova3")
+                sommaPeriodoSecondi += durataInt[i]*3600
+            elif i == 4:
+                #min
+                print("prova4")
+                sommaPeriodoSecondi += durataInt[i]*60
+            elif i == 5:
+                #sec
+                print("prova5")
+                sommaPeriodoSecondi += durataInt[i]
+            else:
+                print("else")
+        i += 1
+
+    durataIntervallo=str(sommaPeriodoSecondi)
     valoreMisurato=str(1)
     #per ogni messaggio aggiungiamo: seriale, data inizio intervallo, data fine intervallo, dimensione intervallo (in s) valore misurazione in intervallo
 
